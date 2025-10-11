22fb9b0487ab5437c939c9483f178cd3b95e5532
Ssuwimon <maynp.08@gmail.com>
2025-10-12 02:04:13 +0700

    Fanall

---
+++ Final/lab1/lab 1.2 .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    char user[60];
+    int year;
+    
+    printf("Enter your name please: ");
+    scanf("%s", user);
+    printf("Enter your age please: ");
+    scanf("%d", &year);
+
+    printf("------------\n");
+    printf("Hello %s\n", user);
+    printf("Age = %d\n", year);
+
+    return 0;
+}
