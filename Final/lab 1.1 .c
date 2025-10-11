a7d5362e60236f260839fb12b19d8bf46a0583d2
Ssuwimon <maynp.08@gmail.com>
2025-10-11 23:52:28 +0700

    final1.1

---
+++ Final/lab 1.1 .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, z, ans;
+    printf("ใส่เลขมาสามตัวเลยจ้า: ");
+    scanf("%d %d %d",&x,&y,&z);
+    ans = (x+y+z);
+    printf("Answer = %d", ans);
+    return 0;
+}
